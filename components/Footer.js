cbb9a9c229a2b8dd5a52a92e5fb76be270f5f015
Mark-R-5388 <markrose@markwebdev.com>
2022-02-17 17:38:58 -0600

    home complete about layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,41 +1,36 @@
-import Link from 'next/link';
-import { BsLinkedin, BsTwitter, BsGithub } from 'react-icons/bs';
-import styles from './Footer.module.css';
+import Link from "next/link";
+import { BsLinkedin, BsTwitter, BsGithub } from "react-icons/bs";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
-	return (
-		<footer className={styles.footer}>
-			<div className={styles.footer_container}>
-				<p className={styles.footer_text}>
-					Mark Rose Web Developer &copy; 2022
-				</p>
-				<div className={styles.footer_contact_container}>
-					<div className={styles.footer_contact_email_container}>
-						<p className={styles.footer_contact_email}>
-							markrose@markwebdev.com
-						</p>
-					</div>
-					<div className={styles.footer_contact_social_container}>
-						<Link href='https://github.com'>
-							<a className={styles.footer_social_link}>
-								<BsGithub />
-							</a>
-						</Link>
-						<Link href='https://linkdn.com'>
-							<a className={styles.footer_social_link}>
-								<BsLinkedin />
-							</a>
-						</Link>
-						<Link href='https://twitter.com'>
-							<a className={styles.footer_social_link}>
-								<BsTwitter />
-							</a>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>Mark Rose Web Developer &copy; 2022</p>
+        <div className={styles.contact_container}>
+          <p className={styles.contact_email}>markrose@markwebdev.com</p>
+
+          <div className={styles.contact_social_container}>
+            <Link href="https://github.com">
+              <a className={styles.social_link}>
+                <BsGithub />
+              </a>
+            </Link>
+            <Link href="https://linkdn.com">
+              <a className={styles.social_link}>
+                <BsLinkedin />
+              </a>
+            </Link>
+            <Link href="https://twitter.com">
+              <a className={styles.social_link}>
+                <BsTwitter />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,25 @@
-import Link from 'next/link';
-import styles from './Header.module.css';
+import Link from "next/link";
+import styles from "./Header.module.scss";
 
 const Header = () => {
-	return (
-		<header className={styles.header}>
-			<nav className={styles.header_nav_container}>
-				<Link href='/'>
-					<a className={styles.header_nav_link}>Home</a>
-				</Link>
-				<Link href='/about'>
-					<a className={styles.header_nav_link}>About</a>
-				</Link>
-				<Link href='/portfolio'>
-					<a className={styles.header_nav_link}>Portfolio</a>
-				</Link>
-				<Link href='/contact'>
-					<a className={styles.header_nav_link}>Contact</a>
-				</Link>
-			</nav>
-		</header>
-	);
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav_container}>
+        <Link href="/">
+          <a className={styles.nav_link}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={styles.nav_link}>About</a>
+        </Link>
+        <Link href="/portfolio">
+          <a className={styles.nav_link}>Portfolio</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.nav_link}>Contact</a>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
